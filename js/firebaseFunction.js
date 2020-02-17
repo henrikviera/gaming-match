87e3de0ef7cab5fafd4f553e84e94a5627dbf64a
Mikkel Beck Knudsen <mikkel1a2@gmail.com>
2020-02-17 11:33:50 +0100

    add user done

--- js/firebaseFunction.js
+++ js/firebaseFunction.js
@@ -8,9 +8,9 @@ var firebaseConfig = {
     messagingSenderId: "408389194553",
     appId: "1:408389194553:web:6d30c2a496a7774ec50ee8"
   };
-  // Initialize Firebase
+  // Initialize Firebase 
   firebase.initializeApp(firebaseConfig);
-
+//---------------------------------------- sign up ----------------------------------------
 function signUp(){
 let email = document.querySelector('#mail').value;
 let password = document.querySelector('#password').value;
@@ -21,4 +21,40 @@ firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(e
     var errorMessage = error.message;
     // ...
   });
-}
+  db.collection("users").add({
+  email: email
+})
+.then(function(docRef) {
+    console.log("Document written with ID: ", docRef.id);
+})
+.catch(function(error) {
+    console.error("Error adding document: ", error);
+});
+// ------------------------------  sign in  -------------------------------------------------
+};
+// function signIn(){
+//   let email = document.querySelector('#inMail').value;
+//   let password = document.querySelector('#inPassword').value;
+//   firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+//     // Handle Errors here.
+//     var errorCode = error.code;
+//     var errorMessage = error.message;
+//     // ...
+//   });email-password.html
+//   firebase.auth().onAuthStateChanged(function(user) {
+//     if (user) {
+//       // User is signed in.
+//       var displayName = user.displayName;
+//       var email = user.email;
+//       var emailVerified = user.emailVerified;
+//       var photoURL = user.photoURL;
+//       var isAnonymous = user.isAnonymous;
+//       var uid = user.uid;
+//       var providerData = user.providerData;
+//       // ...
+//     } else {
+//       // User is signed out.
+//       // ...
+//     }
+//   });
+// }
